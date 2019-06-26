f57bfddbf75921d525f902703d9f208a66889215
chiwoo <qkrcldn12@gmail.com>
2019-06-26 13:27:24 +0900

    Completed V1.0

--- AzurePowerShell/PCW-GetAzLoadBalancers/src/01_main.ps1
+++ AzurePowerShell/PCW-GetAzLoadBalancers/src/01_main.ps1
@@ -1,10 +1,74 @@
 $location = (Get-Location).Path
-Import-module -Name "$location\model.psm1"
+Import-module -Name "$location\src\library\tools.psm1"
 
-# making table
-$col=@("balancer", "kind", "ip", "backendPool", "nic", "probe", "rule")
-$table = MakeTable "balancer" $col
+$col=@("lbName", "lbKind", "frontEndIP", "backEndPoolName", "backEndPool", "probeRule", "ruleProto", "rulePort")
+$table = MakeTable "test" $col
 
 $lbList = Get-AzLoadBalancer
+$vms=Get-AzVM
 
-$row = $table.NewRow()
+
+foreach ($lb in $lbList) {
+    $lbName = $lb.Name
+    $lbKind = ""
+    $lbIPs = ""
+    $frontEndIP = ""
+    $lbBackEndName = ""
+    $probeRule = ""
+    if ($lb.FrontendIpConfigurations[0].PublicIpAddress -eq $null) {
+        $lbKind = "private"
+    } else {
+        $lbKind = "public"
+        $lbIPS = "public"
+    }
+    foreach ($lbRule in $lb.LoadBalancingRules) {
+        $backendPool = ""
+        $FrontIPId = ($lbRule.FrontendIPConfiguration.Id | Select-String  $lbList.FrontendIpConfigurations.Id).ToString()
+
+        ## Searching for FrontEndIP
+        foreach ($fronttmp in $lb.FrontendIpConfigurations) {
+            #$fronttmp
+            if ($fronttmp.Id -eq $FrontIPId) {
+                if ($fronttmp.PrivateIpAddress -ne $null) {
+                    $frontEndIP = $fronttmp.PrivateIpAddress
+                } elseif ($fronttmp.PublicIpAddress -ne $null) {
+                    $frontEndIP = $fronttmp.PublicIpAddress
+                } else {
+                    $frontEndIP = $fronttmp.PrivateIpAddress + " : " + $fronttmp.PublicIpAddress
+                }
+            }
+        }
+
+        foreach ($lbBackEndId in $lb.BackendAddressPools) {
+            if ($lbrule.BackendAddressPool.ID -eq $lbBackEndId.Id) {
+                $lbBackEndName = $lbBackEndId.Name
+                foreach ($nic in $lbBackEndId.BackendIpConfigurations.Id) {
+                    $tt = $nic -match "/networkInterfaces/(?<nicName>.+)/ipConfigurations/"
+                    $backendPool += (findVMNameWithNICName $Matches.nicName $vms) + " "
+                }
+            }
+        }
+
+        foreach ($probeId in $lb.Probes) {
+            if ($probeId.Id -eq $lbRule.Probe.Id) {
+                $probeRule = $ProbeId.Protocol + " : " +$ProbeId.Port
+            }
+        }
+
+        $ruleProtocol = $lbRule.Protocol
+        $rulePort = [String]$lbRule.FrontendPort + " => " + [String]$lbRule.BackendPort
+        
+        $row = $table.NewRow()
+        $row.lbName = $lbName
+        $row.lbKind = $lbKind
+        $row.frontEndIP = $frontEndIP
+        $row.backEndPoolName = $lbBackEndName
+        $row.backEndPool = $backendPool
+        $row.probeRule= $probeRule
+        $row.ruleProto = $ruleProtocol
+        $row.rulePort = $rulePort
+        $table.Rows.Add($row)
+    }
+}
+
+$table
--- AzurePowerShell/PCW-GetAzLoadBalancers/src/logic-test.ps1
+++ AzurePowerShell/PCW-GetAzLoadBalancers/src/logic-test.ps1
@@ -1,12 +1,12 @@
 $location = (Get-Location).Path
-Import-module -NAme "$location\model\model.psm1"
+Import-module -Name "$location\src\library\tools.psm1"
 
-$col=@("a", "b", "c")
+$col=@("lbName", "lbKind", "frontEndIP", "backEndPoolName", "backEndPool", "ruleProto", "rulePort")
 $table = MakeTable "test" $col
 $row = $table.NewRow()
 
 $lbList = Get-AzLoadBalancer
-$vm=Get-AzVM
+$vms=Get-AzVM
 
 $lists = ($vm.NetworkProfile.NetworkInterfaces.Id -match "/Microsoft.Network/networkInterfaces/(?<vmNIC>.+)")
 
@@ -40,10 +40,14 @@ foreach ($lbProbe in $lb.Probes) {
     $probePort = $lbProbe.Port
 }
 
+
 foreach ($lb in $lbList) {
     $lbName = $lb.Name
     $lbKind = ""
     $lbIPs = ""
+    $frontEndIP = ""
+    $lbBackEndName = ""
+    $probeRule = ""
     if ($lb.FrontendIpConfigurations[0].PublicIpAddress -eq $null) {
         $lbKind = "private"
     } else {
@@ -51,12 +55,43 @@ foreach ($lb in $lbList) {
         $lbIPS = "public"
     }
     foreach ($lbRule in $lb.LoadBalancingRules) {
+        $backendPool = ""
         $FrontIPId = ($lbRule.FrontendIPConfiguration.Id | Select-String  $lbList.FrontendIpConfigurations.Id).ToString()
-        $ruletmp = $lbRule.FrontendIPConfiguration.Id -match "/frontendIPConfigurations/(?<lbFrontIP>.+)"
-        
-        $FrontIP = $Matches.lbFrontIP
+
+        ## Searching for FrontEndIP
+        foreach ($fronttmp in $lb.FrontendIpConfigurations) {
+            #$fronttmp
+            if ($fronttmp.Id -eq $FrontIPId) {
+                if ($fronttmp.PrivateIpAddress -ne $null) {
+                    $frontEndIP = $fronttmp.PrivateIpAddress
+                } elseif ($fronttmp.PublicIpAddress -ne $null) {
+                    $frontEndIP = $fronttmp.PublicIpAddress
+                } else {
+                    $frontEndIP = $fronttmp.PrivateIpAddress + " : " + $fronttmp.PublicIpAddress
+                }
+            }
+        }
+
+        foreach ($lbBackEndId in $lb.BackendAddressPools) {
+            if ($lbrule.BackendAddressPool.ID -eq $lbBackEndId.Id) {
+                $lbBackEndName = $lbBackEndId.Name
+                foreach ($nic in $lbBackEndId.BackendIpConfigurations.Id) {
+                    $tt = $nic -match "/networkInterfaces/(?<nicName>.+)/ipConfigurations/"
+                    $backendPool += (findVMNameWithNICName $Matches.nicName $vms) + " "
+                }
+            }
+        }
+
+        foreach ($probeId in $lb.Probes) {
+            if ($probeId.Id -eq $lbRule.Probe.Id) {
+                $probeRule = $ProbeId.Protocol + " " +$ProbeId.Port
+            }
+        }
+        $probeProto
+        $probePort
         $ruleProtocol = $lbRule.Protocol
-        $ruleFrontEndPort = $lbRule.FrontendPort
-        $ruleBackEndPort = $lbRule.BackendPort
+        $rulePort = [String]$lbRule.FrontendPort + " => " + [String]$lbRule.BackendPort
+
+        Write-Host $lbName $lbKind $frontEndIP $lbBackEndName $backendPool $probeRule $ruleProtocol $rulePort
     }
 }
