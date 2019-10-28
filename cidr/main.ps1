8fcc52fe2e901b52fc43da680557d911f13b31a0
chiwoo <qkrcldn12@gmail.com>
2019-10-28 15:23:36 +0900

    update readme

--- cidr/main.ps1
+++ cidr/main.ps1
@@ -1,13 +1,7 @@
-Import-Module -Name D:\PowerShell\study\cidr\cidr.psm1 -Force
-## $bbb = getCidrCalculator
-$bbb.setCidr("10.3.5.3/31")
-$bbb.setStartToEndIP()
-$bbb.getStartIP()
-$bbb.getEndIP()
-$bbb.getHostCount()
-
-$a = 8
-
-if($a -ge 8 -and $a -le 15) {
-    Write-Host Ho
-}
+Import-Module -Name D:\PowerShell\PowerShell\cidr\cidr.psm1 -Force
+$cidr = getCidrCalculator
+$cidr.setCidr("10.0.0.1/30")
+$cidr.setStartToEndIP()
+$cidr.getStartIP()
+$cidr.getEndIP()
+$cidr.getHostCount()
