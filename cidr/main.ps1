dcb00aa561229058a627f995d5fc8191e930a629
chiwoo <qkrcldn12@gmail.com>
2019-10-28 17:13:03 +0900

    upload images

--- cidr/main.ps1
+++ cidr/main.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name D:\PowerShell\PowerShell\cidr\cidr.psm1 -Force
 $cidr = getCidrCalculator
-$cidr.setCidr("10.0.0.1/30")
+$cidr.setCidr("10.1.3.1/14")
 $cidr.setStartToEndIP()
 $cidr.getStartIP()
 $cidr.getEndIP()
